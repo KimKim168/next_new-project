6afcb89736a3bddf739a5f7b62890bb514979379
kimkim168 <KimKim168>
2025-03-07 16:23:08 +0700

    imageHostnam

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,10 @@ const nextConfig = {
     images:{
         remotePatterns:[
             {
-                hostname:'images.unsplash.com',
-                hostname: "127.0.0.1",
-                port: "8000",
+                // hostname:'images.unsplash.com',
+                // hostname: "127.0.0.1",
+                // port: "8000",
+                hostname:'rule.kimsoreya.site',
 
             }
         ]
